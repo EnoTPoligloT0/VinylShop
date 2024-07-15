8b9ccfebf703aba09b0aeebfffca01c96a949078
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-07-15 06:50:41 +0200

    Created Vinyl Entity

---
+++ VinylShop.DataAccess/Entities/VinylEntity.cs
@@ -0,0 +1,19 @@
+using VinylShop.Core.Models;
+
+namespace VinylShop.DataAccess.Entities;
+
+public class VinylEntity
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public int ReleaseYear { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public bool IsAvailable { get; set; }
+
+    public Guid OrderItemId { get; set; }
+    public OrderItem? OrderItem { get; set; }
+}
