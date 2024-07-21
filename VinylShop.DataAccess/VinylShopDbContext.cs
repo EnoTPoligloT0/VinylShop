6b78a9af4f855a12815a064f32af14fc3f2b2283
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-07-21 22:46:18 +0200

    Configure VinylEntity with properties and relationships

--- VinylShop.DataAccess/VinylShopDbContext.cs
+++ VinylShop.DataAccess/VinylShopDbContext.cs
@@ -25,6 +25,24 @@ public class VinylShopDbContext : DbContext
     
     public ILoggerFactory CreateLoggerFactory() =>
         LoggerFactory.Create(builder => { builder.AddConsole(); });
-    
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<VinylEntity>(vinylBuilder =>
+        {
+            vinylBuilder.ToTable("Vinyls").HasKey(v => v.Id);
+            vinylBuilder.Property(v => v.Title).IsRequired().HasMaxLength(100);
+            vinylBuilder.Property(v => v.Artist).IsRequired().HasMaxLength(100);
+            vinylBuilder.Property(v => v.Genre).IsRequired().HasMaxLength(50);
+            vinylBuilder.Property(v => v.ReleaseYear).IsRequired();
+            vinylBuilder.Property(v => v.Price).IsRequired().HasColumnType("decimal(18,2)");
+            vinylBuilder.Property(v => v.Stock).IsRequired();
+            vinylBuilder.Property(v => v.Description).HasMaxLength(500);
+            vinylBuilder.Property(v => v.IsAvailable).IsRequired();
+            vinylBuilder.HasOne(v => v.OrderItem)
+                .WithMany(o => o.Vinyls)
+                .HasForeignKey(v => v.OrderItemId);
+        });
+
+    }
 }
