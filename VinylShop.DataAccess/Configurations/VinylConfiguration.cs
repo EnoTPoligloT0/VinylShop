6c8fca30f56dc6b3e1599a8ea89be7fe1600db86
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-07-22 05:29:43 +0200

    Moved VinylConfiguration from ApplicationDbContext to Configuration folder

---
+++ VinylShop.DataAccess/Configurations/VinylConfiguration.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VinylShop.DataAccess.Entities;
+
+namespace VinylShop.DataAccess.Configurations;
+
+public class VinylConfiguration : IEntityTypeConfiguration<VinylEntity>
+{
+    public void Configure(EntityTypeBuilder<VinylEntity> vinylBuilder)
+    {
+        vinylBuilder.ToTable("Vinyls").HasKey(v => v.Id);
+        vinylBuilder.Property(v => v.Title).IsRequired().HasMaxLength(100);
+        vinylBuilder.Property(v => v.Artist).IsRequired().HasMaxLength(100);
+        vinylBuilder.Property(v => v.Genre).IsRequired().HasMaxLength(50);
+        vinylBuilder.Property(v => v.ReleaseYear).IsRequired();
+        vinylBuilder.Property(v => v.Price).IsRequired().HasColumnType("decimal(18,2)");
+        vinylBuilder.Property(v => v.Stock).IsRequired();
+        vinylBuilder.Property(v => v.Description).HasMaxLength(500);
+        vinylBuilder.Property(v => v.IsAvailable).IsRequired();
+        vinylBuilder.HasOne(v => v.OrderItem)
+            .WithMany(o => o.Vinyls)
+            .HasForeignKey(v => v.OrderItemId);
+    }
+}
