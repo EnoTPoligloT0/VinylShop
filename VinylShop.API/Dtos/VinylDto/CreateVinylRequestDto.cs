3726788e4edba17b07531a64cfdbfb574d45f7ac
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-03 23:56:59 +0200

    Implemented CreateVinylRequestDto

---
+++ VinylShop.API/Dtos/VinylDto/CreateVinylRequestDto.cs
@@ -0,0 +1,14 @@
+namespace VinylShop.API.Dtos.VinylDto;
+
+public class CreateVinylRequestDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public int ReleaseYear { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public bool IsAvailable { get; set; }
+    public Guid OrderItemId { get; set; }
+}
