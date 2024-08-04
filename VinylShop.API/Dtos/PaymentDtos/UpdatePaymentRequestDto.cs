8738b76370a0bfd312370c178278052e0a04f15f
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-04 16:12:41 +0200

    Implemented UpdatePaymentRequestDto

---
+++ VinylShop.API/Dtos/PaymentDtos/UpdatePaymentRequestDto.cs
@@ -0,0 +1,8 @@
+namespace VinylShop.API.Dtos.PaymentDtos;
+
+public class UpdatePaymentRequestDto
+{
+    public DateTime PaymentDate { get; set; }
+    public decimal Amount { get; set; }
+    public string PaymentMethod { get; set; } = string.Empty;
+}
