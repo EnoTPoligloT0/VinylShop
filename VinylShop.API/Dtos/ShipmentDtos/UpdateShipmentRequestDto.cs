f51e70d205e8b325536cece1ac24ad528ceb98e8
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-04 16:14:26 +0200

    Implemented UpdateShipmentRequestDto

---
+++ VinylShop.API/Dtos/ShipmentDtos/UpdateShipmentRequestDto.cs
@@ -0,0 +1,8 @@
+namespace VinylShop.API.Dtos.ShipmentDtos;
+
+public class UpdateShipmentRequestDto
+{
+    public DateTime ShipmentDate { get; set; }
+    public string TrackingNumber { get; set; } = string.Empty;
+    public string ShipmentStatus { get; set; } = string.Empty;
+}
