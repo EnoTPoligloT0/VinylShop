53916e23ffd55bd25f26e8b118b219af8763fb6b
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-04 16:13:52 +0200

    Created ShipmentDto

---
+++ VinylShop.API/Dtos/ShipmentDtos/ShipmentDto.cs
@@ -0,0 +1,10 @@
+namespace VinylShop.API.Dtos.ShipmentDtos;
+
+public class ShipmentDto
+{
+    public Guid ShipmentId { get; set; }
+    public Guid OrderId { get; set; }
+    public DateTime ShipmentDate { get; set; }
+    public string TrackingNumber { get; set; } = string.Empty;
+    public string ShipmentStatus { get; set; } = string.Empty;
+}
