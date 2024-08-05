22e3e719e3b9bfd4eb371d1a54f81b42436b128c
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-05 23:45:05 +0200

    Implemented IOrderItemRepository

---
+++ VinylShop.Core/Interfaces/Repositories/IOrderItemRepository.cs
@@ -0,0 +1,13 @@
+using VinylShop.Core.Dtos.OrderItemDtos;
+using VinylShop.Core.Models;
+
+namespace VinylShop.Core.Interfaces.Repositories;
+
+public interface IOrderItemRepository
+{
+    Task Create(OrderItem orderItem);
+    Task Delete(Guid id);
+    Task<List<OrderItem>> Get();
+    Task<OrderItem> GetById(Guid id);
+    Task Update(Guid id, UpdateOrderItemRequestDto updateOrderItemRequestDto);
+}
