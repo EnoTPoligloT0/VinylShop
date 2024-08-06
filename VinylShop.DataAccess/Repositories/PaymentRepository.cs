2fabad1c576cca53deab9401d61f75d91b0a5e28
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-06 23:57:49 +0200

    Implemented Payment Repository with CRUD operations

---
+++ VinylShop.DataAccess/Repositories/PaymentRepository.cs
@@ -0,0 +1,72 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using VinylShop.Core.Interfaces.Repositories;
+using VinylShop.Core.Models;
+using VinylShop.DataAccess.Entities;
+
+namespace VinylShop.DataAccess.Repositories;
+
+public class PaymentRepository : IPaymentRepository
+{
+    private readonly VinylShopDbContext _context;
+    private readonly IMapper _mapper;
+
+    public PaymentRepository(VinylShopDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    
+    public async Task Create(Payment payment)
+    {
+        var paymentEntity = new PaymentEntity
+        {
+            PaymentId = payment.PaymentId,
+            OrderId = payment.OrderId,
+            PaymentDate = payment.PaymentDate,
+            Amount = payment.Amount,
+            PaymentMethod = payment.PaymentMethod
+        };
+        await _context.Payment.AddAsync(paymentEntity);
+        await _context.SaveChangesAsync();
+    }
+
+
+
+    public async Task<List<Payment>> Get()
+    {
+        var paymentEntities = await _context.Payment
+            .AsNoTracking()
+            .ToListAsync();
+
+        return _mapper.Map<List<Payment>>(paymentEntities);
+    }
+
+    public async Task<Payment> GetById(Guid id)
+    {
+        var paymentEntity = await _context.Payment
+            .AsNoTracking()
+            .SingleOrDefaultAsync(c => c.PaymentId == id);
+        
+        return _mapper.Map<Payment>(paymentEntity);
+    }
+
+    public async Task Update(Guid id, decimal amount, string paymentMethod)
+    {
+        await _context.Payment
+            .Where(c => c.PaymentId == id)
+            .ExecuteUpdateAsync(
+                s => s
+                    .SetProperty(c => c.Amount, amount)
+                    .SetProperty(c => c.PaymentMethod, paymentMethod)
+            );
+    }
+
+    public async Task Delete(Guid id)
+    {
+        await _context.Payment
+            .Where(c => c.PaymentId == id)
+            .ExecuteDeleteAsync();
+    }
+ 
+}
