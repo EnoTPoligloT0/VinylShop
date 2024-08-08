0b9bae91aac9b8078aed93ececb28815615ca5ab
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-08 22:27:17 +0200

    Implemented Shipment Service interface

---
+++ VinylShop.Core/Interfaces/Services/IShipmentService.cs
@@ -0,0 +1,12 @@
+using VinylShop.Core.Models;
+
+namespace VinylShop.Core.Interfaces.Services;
+
+public interface IShipmentService
+{
+    Task CreateShipment(Shipment shipment);
+    Task DeleteShipment(Guid id);
+    Task<List<Shipment>> GetShipments();
+    Task<Shipment> GetShipmentById(Guid id);
+    Task UpdateShipment(Guid id, string trackingNumber, string shipmentStatus);
+}
