23c3e7badc1a4030fa412cf9f6ab58ad28645cf1
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-08 22:27:29 +0200

    Implemented Payment Service interface

---
+++ VinylShop.Core/Interfaces/Services/IPaymentService.cs
@@ -0,0 +1,12 @@
+using VinylShop.Core.Models;
+
+namespace VinylShop.Core.Interfaces.Services;
+
+public interface IPaymentService
+{
+    Task CreatePayment(Payment payment);
+    Task DeletePayment(Guid id);
+    Task<List<Payment>> GetPayments();
+    Task<Payment> GetPaymentById(Guid id);
+    Task UpdatePayment(Guid id, decimal amount, string paymentMethod);
+}
