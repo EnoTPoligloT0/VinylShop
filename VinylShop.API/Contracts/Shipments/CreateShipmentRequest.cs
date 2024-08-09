8c97e32608502261b6f0e2ccdd87c6fbb31fe280
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-09 02:38:14 +0200

    Refactored CreateShipmentRequest to record

--- VinylShop.API/Contracts/Shipments/CreateShipmentRequest.cs
+++ VinylShop.API/Contracts/Shipments/CreateShipmentRequest.cs
@@ -1,9 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VinylShop.API.Contracts.Shipments;
 
-public class CreateShipmentRequest
-{
-    public Guid OrderId { get; set; }
-    public DateTime ShipmentDate { get; set; }
-    public string TrackingNumber { get; set; } = string.Empty;
-    public string ShipmentStatus { get; set; } = string.Empty;
-}
+public record CreateShipmentRequest(
+    [Required] Guid OrderId,
+    [Required] DateTime ShipmentDate,
+    [Required] string TrackingNumber,
+    [Required] string ShipmentStatus
+);
