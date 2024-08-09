fb9d55e5d40218679748537f4eaa45aae517d9ea
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-09 02:30:20 +0200

    Refactored CreateOrderRequest to record contract

--- VinylShop.API/Contracts/Orders/CreateOrderRequest.cs
+++ VinylShop.API/Contracts/Orders/CreateOrderRequest.cs
@@ -1,11 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using VinylShop.API.Contracts.OrderItems;
 
 namespace VinylShop.API.Contracts.Orders;
 
-public class CreateOrderRequest
-{
-    public Guid UserId { get; set; }
-    public DateTime OrderDate { get; set; }
-    public decimal TotalAmount { get; set; }
-    public List<CreateOrderItemRequest> OrderItems { get; set; } = new List<CreateOrderItemRequest>();
-}
+public record CreateOrderRequest(
+    [Required] Guid UserId,
+    [Required] DateTime OrderDate,
+    [Required] decimal TotalAmount,
+    [Required] List<CreateOrderItemRequest> OrderItems
+);
