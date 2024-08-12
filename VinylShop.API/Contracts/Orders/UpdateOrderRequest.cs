c0d8f16bc6923cfed964c8074a14c708da7ca2a0
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-12 23:07:49 +0200

    Refactored UpdateOrderRequest to record contract

--- VinylShop.API/Contracts/Orders/UpdateOrderRequest.cs
+++ VinylShop.API/Contracts/Orders/UpdateOrderRequest.cs
@@ -2,9 +2,9 @@ using VinylShop.API.Contracts.OrderItems;
 
 namespace VinylShop.API.Contracts.Orders;
 
-public class UpdateOrderRequest
-{
-    public DateTime OrderDate { get; set; }
-    public decimal TotalAmount { get; set; }
-    public List<UpdateOrderItemRequest> OrderItems { get; set; } = new List<UpdateOrderItemRequest>();
-}
+public record UpdateOrderRequest
+(
+     DateTime OrderDate,
+     decimal TotalAmount,
+     List<UpdateOrderItemRequest> OrderItems
+);
