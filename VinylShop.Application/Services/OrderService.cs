be8b5c9bf9911bb1040e84d8d3837eaa26e076d7
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-13 23:53:46 +0200

    Implemented OrderService with CRUD operations on repository

---
+++ VinylShop.Application/Services/OrderService.cs
@@ -0,0 +1,40 @@
+using VinylShop.Core.Interfaces.Repositories;
+using VinylShop.Core.Interfaces.Services;
+using VinylShop.Core.Models;
+
+namespace VinylShop.Application.Services;
+
+public class OrderService : IOrderService
+{
+    public readonly IOrderRepository _orderRepository;
+
+    public OrderService(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+
+    public async Task CreateOrder(Order order)
+    {
+        await _orderRepository.Create(order);
+    }
+
+
+    public async Task<List<Order>> GetOrders()
+    {
+        return await _orderRepository.Get();
+    }
+
+    public async Task<Order> GetOrderById(Guid id)
+    {
+        return await _orderRepository.GetById(id);
+    }
+
+    public async Task UpdateOrder(Guid id, DateTime orderDate, decimal totalAmount, List<OrderItem> orderItems)
+    {
+        await _orderRepository.Update(id, orderDate, totalAmount, orderItems);
+    }
+    public async Task DeleteOrder(Guid id)
+    {
+        await _orderRepository.Delete(id);
+    }
+}
