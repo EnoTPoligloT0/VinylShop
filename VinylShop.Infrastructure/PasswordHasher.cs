20996e95b1d3417857d9cd7ba6c7a1e22db7a01d
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-14 22:45:13 +0200

    PasswordHasher class with Generate and Verify methods

---
+++ VinylShop.Infrastructure/PasswordHasher.cs
@@ -0,0 +1,14 @@
+namespace VinylShop.Infrastructure;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string Generate(string password)
+    {
+        return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+    }
+
+    public bool Verify(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
