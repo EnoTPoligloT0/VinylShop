ee6dfbd2e9a84f642d28bd57a5694bb0f94f1bfc
EnoTPoligloT <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-14 18:16:57 +0200

    Added all entities by DbSet and applied all configurations

--- VinylShop.DataAccess/VinylShopDbContext.cs
+++ VinylShop.DataAccess/VinylShopDbContext.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using VinylShop.DataAccess.Configurations;
 using VinylShop.DataAccess.Entities;
 
 namespace VinylShop.DataAccess;
@@ -18,16 +19,31 @@ public class VinylShopDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder
-            .UseNpgsql(_configuration.GetConnectionString("Database"))
-            .UseLoggerFactory(CreateLoggerFactory())
+            .UseNpgsql(_configuration.GetConnectionString("DefaultConnection"))
+            .UseLoggerFactory(CreateLoggerFactory()) 
             .EnableSensitiveDataLogging();
     }
+    
+    public DbSet<OrderEntity> Orders { get; set; }
+    public DbSet<OrderItemEntity> OrderItems { get; set; }
+    public DbSet<PaymentEntity> Payment { get; set; }
+    public DbSet<ShipmentEntity> Shipment { get; set; }
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<VinylEntity> Vinyls { get; set; }
 
     public ILoggerFactory CreateLoggerFactory() =>
-        LoggerFactory.Create(builder => { builder.AddConsole(); });
+        LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole(); 
+        });
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(VinylShopDbContext).Assembly);
+        modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
+        modelBuilder.ApplyConfiguration(new VinylConfiguration());
+        modelBuilder.ApplyConfiguration(new ShipmentConfiguration());
+        modelBuilder.ApplyConfiguration(new PaymentConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
     }
 }
