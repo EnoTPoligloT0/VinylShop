73878e6302d924a487116d63c67d63e34264a2ff
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-15 23:43:18 +0200

    Jwt Provider with Generate token realization

---
+++ VinylShop.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,38 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using VinylShop.Application.Auth;
+using VinylShop.Core.Models;
+
+namespace VinylShop.Infrastructure.Authentication;
+
+public class JwtProvider : IJwtProvider
+{
+    private readonly JwtOptions _options;
+
+    public JwtProvider(IOptions<JwtOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public string Generate(User user)
+    {
+        Claim[] claims = [new("userId", user.UserId.ToString())];
+        
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+            SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            signingCredentials: signingCredentials,
+            expires: DateTime.UtcNow.AddHours(_options.ExpiresHours));
+
+        var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return tokenValue;
+    }
+    
+}
