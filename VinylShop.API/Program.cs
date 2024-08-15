62e68daf414248d5f3a2f948cc855bf5391de949
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-15 23:50:55 +0200

    Serilog Package with UseSerilog method

--- VinylShop.API/Program.cs
+++ VinylShop.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using VinylShop.Application.Services;
 using VinylShop.Core.Interfaces.Repositories;
 using VinylShop.Core.Interfaces.Services;
@@ -9,6 +10,8 @@ using VinylShop.DataAccess.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((context, loggerConfig) =>
+    loggerConfig.ReadFrom.Configuration(context.Configuration));
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 var services = builder.Services; 
