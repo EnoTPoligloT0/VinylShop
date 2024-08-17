ca99a7e341e6843b41aed2d9cb03525556f2efd0
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-17 02:03:09 +0200

    Add global exception handler for centralized error logging and response

---
+++ VinylShop.API/Infrastructure/GlobalExceptionHandler.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VinylShop.API.Infrastructure;
+
+public class GlobalExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Exception occured: {Message}", exception.Message);
+
+        httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var problemDetails = new ProblemDetails()
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Server Error",
+            Detail = exception.Message
+        };
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
