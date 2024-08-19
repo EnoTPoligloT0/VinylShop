2259ffcb44fd8060b05b2b799a25f6ad981a3ac0
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-19 22:46:38 +0200

    Deleted OrderItemId to get only Vinyl

--- VinylShop.API/Contracts/Vinyls/GetVinylResponse.cs
+++ VinylShop.API/Contracts/Vinyls/GetVinylResponse.cs
@@ -3,7 +3,6 @@ namespace VinylShop.API.Contracts.Vinyls;
 public class GetVinylResponse
 {
     public Guid Id { get; set; }
-    public Guid OrderItemId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Artist { get; set; } = string.Empty;
     public string Genre { get; set; } = string.Empty;
