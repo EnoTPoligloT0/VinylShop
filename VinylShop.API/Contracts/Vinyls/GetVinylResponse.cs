33222a962ea91594946c63ba6c9d58d49c20bf98
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-19 21:09:04 +0200

    Moved OrderItemId field after Id

--- VinylShop.API/Contracts/Vinyls/GetVinylResponse.cs
+++ VinylShop.API/Contracts/Vinyls/GetVinylResponse.cs
@@ -3,6 +3,7 @@ namespace VinylShop.API.Contracts.Vinyls;
 public class GetVinylResponse
 {
     public Guid Id { get; set; }
+    public Guid OrderItemId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Artist { get; set; } = string.Empty;
     public string Genre { get; set; } = string.Empty;
@@ -11,5 +12,4 @@ public class GetVinylResponse
     public int Stock { get; set; }
     public string Description { get; set; } = string.Empty;
     public bool IsAvailable { get; set; }
-    public Guid OrderItemId { get; set; }
 }
