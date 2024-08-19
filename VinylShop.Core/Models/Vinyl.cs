a91e9378df4b707b3879cd14f16759220c372008
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-20 00:04:30 +0200

    Refactored tabs

--- VinylShop.Core/Models/Vinyl.cs
+++ VinylShop.Core/Models/Vinyl.cs
@@ -5,11 +5,8 @@ namespace VinylShop.Core.Models;
 
 public class Vinyl
 {
-    public const int MAX_TITLE_LENGHT = 250;
-
     public Guid Id { get; }
     public Guid OrderItemId { get; }
-    
     public OrderItem? OrderItem { get; }
     public string Title { get; } = string.Empty;
     public string Artist { get; } = string.Empty;
@@ -19,14 +16,10 @@ public class Vinyl
     public int Stock { get; }
     public string Description { get; } = string.Empty;
     public bool IsAvailable { get; }
-    
-
-
-    private Vinyl(Guid id, Guid orderItemId, string title, string artist, string genre, int releaseYear, decimal price,
+    private Vinyl(Guid id, string title, string artist, string genre, int releaseYear, decimal price,
         int stock, string description, bool isAvailable)
     {
         Id = id;
-        OrderItemId = orderItemId;
         Title = title;
         Artist = artist;
         Genre = genre;
@@ -39,11 +32,11 @@ public class Vinyl
     }
 
     //todo Validation
-    public static Result<Vinyl> Create(Guid vinylId, Guid orderItemId, string title, string artist, string genre,
+    public static Result<Vinyl> Create(Guid vinylId, string title, string artist, string genre,
         int releaseYear, decimal price, int stock, string description, bool isAvailable)
     {
         var vinyl = new Vinyl(
-            vinylId, orderItemId,title, artist, genre, releaseYear, price, stock, description, isAvailable);
+            vinylId, title, artist, genre, releaseYear, price, stock, description, isAvailable);
 
         return Result.Success(vinyl);
     }
