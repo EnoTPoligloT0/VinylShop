df82a6c60f4d41c88a99b99d639c6939e62c319b
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-08-24 23:30:11 +0200

    Added VinylId field

--- VinylShop.API/Contracts/OrderItems/CreateOrderItemRequest.cs
+++ VinylShop.API/Contracts/OrderItems/CreateOrderItemRequest.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace VinylShop.API.Contracts.OrderItems;
 
 public record CreateOrderItemRequest(
+    [Required] Guid VinylId,
     [Required] int Quantity,
     [Required] decimal UnitPrice
 );
