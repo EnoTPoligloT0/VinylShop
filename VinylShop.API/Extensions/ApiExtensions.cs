e3f6d2f8556b8e871d0739a8e8926b90cce4553b
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-09-08 23:45:55 +0200

    Add ApiAuthentication with jwtBearer

--- VinylShop.API/Extensions/ApiExtensions.cs
+++ VinylShop.API/Extensions/ApiExtensions.cs
@@ -1,4 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using VinylShop.API.Endpoints;
+using VinylShop.Infrastructure;
 
 namespace VinylShop.API.Extensions;
 
@@ -12,5 +17,43 @@ public static class ApiExtensions
         app.MapOrderEndpoints();
         app.MapShipmentEndpoints();
         app.MapPaymentEndpoints();
+    }
+
+    public static void AddApiAuthentication(
+        this IServiceCollection services,
+        IOptions<JwtOptions> jwtOptions)
+    {
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtOptions.Value.SecretKey))
+                };
+                
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies["secretCookie"];
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+        services.AddAuthorization();
     }
 }
