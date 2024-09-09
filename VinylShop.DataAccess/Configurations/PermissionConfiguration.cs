6e7797b9b2d1b60978aa02579b0576431566693d
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-09-09 14:08:49 +0200

    Permission entity with configuration

---
+++ VinylShop.DataAccess/Configurations/PermissionConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VinylShop.Core.Enums;
+using VinylShop.DataAccess.Entities;
+
+namespace VinylShop.DataAccess.Configurations;
+
+public partial class PermissionConfiguration : IEntityTypeConfiguration<PermissionEntity>
+{
+    public void Configure(EntityTypeBuilder<PermissionEntity> builder)
+    {
+        builder.HasKey(p => p.Id);
+
+        var permissions = Enum
+            .GetValues<Permission>()
+            .Select(p => new PermissionEntity
+            {
+                Id = (int)p,
+                Name = p.ToString()
+            });
+
+        builder.HasData(permissions);
+    }
+}
