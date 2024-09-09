dd21b7048481c58cfd66e639bf5e528cd5c3bdcb
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-09-09 14:09:35 +0200

    Role entity with configuration

---
+++ VinylShop.DataAccess/Configurations/RoleConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VinylShop.Core.Enums;
+using VinylShop.DataAccess.Entities;
+
+namespace VinylShop.DataAccess.Configurations;
+
+public partial class RoleConfiguration : IEntityTypeConfiguration<RoleEntity>
+{
+    public void Configure(EntityTypeBuilder<RoleEntity> builder)
+    {
+        builder.HasKey(r => r.Id);
+
+        builder.HasMany(r => r.Permissions)
+            .WithMany(p => p.Roles)
+            .UsingEntity<RolePermissionEntity>(
+                l => l.HasOne<PermissionEntity>().WithMany().HasForeignKey(e => e.PermissionId),
+                r => r.HasOne<RoleEntity>().WithMany().HasForeignKey(e => e.RoleId));
+
+        var roles = Enum
+            .GetValues<Role>()
+            .Select(r => new RoleEntity
+            {
+                Id = (int)r,
+                Name = r.ToString()
+            });
+
+        builder.HasData(roles);
+    }
+}
---
+++ VinylShop.DataAccess/Entities/RoleEntity.cs
@@ -0,0 +1,12 @@
+namespace VinylShop.DataAccess.Entities;
+
+public class RoleEntity
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<PermissionEntity> Permissions { get; set; } = [];
+
+    public ICollection<UserEntity> Users { get; set; } = [];
+}
