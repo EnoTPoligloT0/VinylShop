43b5470f0554c0620017ff9d127c08eee31f4b38
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-09-09 14:11:44 +0200

    Authorization Options with RolePermissions

---
+++ VinylShop.DataAccess/AuthorizationOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace VinylShop.DataAccess;
+
+public class AuthorizationOptions
+{
+    public RolePermissions[] RolePermissions { get; set; } = [];
+}
+
+public class RolePermissions
+{
+    public string Role { get; set; } = string.Empty;
+
+    public string[] Permissions { get; set; } = [];
+}
