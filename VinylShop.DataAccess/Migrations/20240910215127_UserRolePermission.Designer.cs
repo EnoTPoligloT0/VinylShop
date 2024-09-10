b3e0d3c326177dfbbb7c541f2f89adff192f4e06
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-09-10 23:52:26 +0200

    PermissionEntityRole new migration

--- VinylShop.DataAccess/Migrations/20240910215127_UserRolePermission.Designer.cs
+++ VinylShop.DataAccess/Migrations/20240910215127_UserRolePermission.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VinylShop.DataAccess;
@@ -11,9 +12,11 @@ using VinylShop.DataAccess;
 namespace VinylShop.DataAccess.Migrations
 {
     [DbContext(typeof(VinylShopDbContext))]
-    partial class VinylShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240910215127_UserRolePermission")]
+    partial class UserRolePermission
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VinylShop.DataAccess/Migrations/20240910215127_UserRolePermission.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VinylShop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRolePermission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PermissionEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PermissionEntityRoleEntity",
+                columns: table => new
+                {
+                    PermissionsId = table.Column<int>(type: "integer", nullable: false),
+                    RolesId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionEntityRoleEntity", x => new { x.PermissionsId, x.RolesId });
+                    table.ForeignKey(
+                        name: "FK_PermissionEntityRoleEntity_PermissionEntity_PermissionsId",
+                        column: x => x.PermissionsId,
+                        principalTable: "PermissionEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PermissionEntityRoleEntity_RoleEntity_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "RoleEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRoleEntity",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RoleId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoleEntity", x => new { x.RoleId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_UserRoleEntity_RoleEntity_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "RoleEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRoleEntity_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PermissionEntityRoleEntity_RolesId",
+                table: "PermissionEntityRoleEntity",
+                column: "RolesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoleEntity_UserId",
+                table: "UserRoleEntity",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PermissionEntityRoleEntity");
+
+            migrationBuilder.DropTable(
+                name: "UserRoleEntity");
+
+            migrationBuilder.DropTable(
+                name: "PermissionEntity");
+
+            migrationBuilder.DropTable(
+                name: "RoleEntity");
+        }
+    }
+}
