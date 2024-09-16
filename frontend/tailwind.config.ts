648542f06dccb325003066b57310dee6bbfe7de8
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-09-16 23:34:47 +0200

    Add colors

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -9,8 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'soft-purple': '#B983FF',  // Lavender Purple
+        'primary-deep-purple': '#7A00CC',  // Deep Purple
+        'deep-purple': '#5A189A', // Alternate Deep Purple
+        'royal-purple': '#4B0082',  // Royal Purple 
+        'warning-yellow': '#FFD700',       // Golden Yellow
+        'light-gray': '#F5F5F5',    // Light Gray
       },
     },
   },
