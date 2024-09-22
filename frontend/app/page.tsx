1be7bc4ffe359753a099a415c04c0d2bba93d97a
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-09-23 00:28:20 +0200

    Include Header to paige

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,11 @@
 import {Hero} from "@/components";
+import React from "react";
+import {Header} from "@/components";
 
 export default function Home() {
   return (
     <main className={"overflow-hidden"}>
+        <Header/>
       <Hero/>
     </main>
   );
--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,5 +1,7 @@
 import Hero from "./Hero";
+import Header from "./Header";  
 
 export {
-    Hero
+    Hero,
+    Header
 }
