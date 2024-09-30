ecca44e5eb4b8c1c56b1ccb5950956b8164bcddd
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-09-30 19:01:01 +0200

    Vinyl type

---
+++ frontend/app/types/vinyl.ts
@@ -0,0 +1,12 @@
+export interface Vinyl {
+    id: string; 
+    title: string;
+    artist: string;
+    genre: string;
+    releaseYear: number;
+    price: number;
+    stock: number;
+    description: string;
+    isAvailable: boolean;
+    imageBase64?: string; 
+}
