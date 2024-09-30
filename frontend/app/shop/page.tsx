38bce38f144ac1b0c8eb77c01fff040292d112aa
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-01 00:56:34 +0200

    Included Search Vinyl into main page

---
+++ frontend/app/shop/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Page() {
+    return (
+        <div className={"container"}></div>
+    );
+}
+
+export default Page;
--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -2,10 +2,12 @@ import Hero from "./Hero";
 import Header from "./Header";  
 import Banner from "./Banner";
 import Categories from "./Categories";
+import SearchVinyl from  "./SearchVinyl"
 
 export {
     Hero,
     Header,
     Banner,
-    Categories
+    Categories,
+    SearchVinyl
 }
