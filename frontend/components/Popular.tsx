90984426e98158f76e54df200c333ef3daa679a4
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-01 02:19:17 +0200

    Popular section

---
+++ frontend/components/Popular.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import VinylList from "@/components/VinylList";
+function Popular() {
+    return (
+        <div>
+            <VinylList/>
+        </div>
+    );
+}
+
+export default Popular;
