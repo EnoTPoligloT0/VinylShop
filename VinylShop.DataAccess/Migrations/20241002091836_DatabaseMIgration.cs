72737547ec0defafbc1da850165dbc27f6df0b49
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-03 00:31:47 +0200

    Database migration on different device

---
+++ VinylShop.DataAccess/Migrations/20241002091836_DatabaseMIgration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+#nullable disable
+
+namespace VinylShop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseMIgration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Image",
+                table: "Vinyls",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Image",
+                table: "Vinyls",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+        }
+    }
+}
