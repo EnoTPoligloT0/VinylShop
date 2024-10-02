3dbe935be2a6343ecf3a45df9d5dad2ddac31526
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-02 04:44:11 +0200

    Image migration back-up

---
+++ VinylShop.DataAccess/Migrations/20240924223110_AddImageToVinyl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VinylShop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageToVinyl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "Vinyls",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Vinyls");
+        }
+    }
+}
