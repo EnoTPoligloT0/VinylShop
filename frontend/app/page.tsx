94d137478ffd65c381470c672beb19a9c386e9a7
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-05 05:27:51 +0200

    Moved Header and Footer to layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,12 +4,10 @@ import {Categories, Hero, Header, Banner, Popular, Footer} from "@/components";
 export default function Home() {
   return (
     <main className={"overflow-hidden"}>
-        <Header/>
       <Hero/>
         <Banner/>
         <Categories/>
         <Popular/>
-        <Footer/>
     </main>
   );
 }
