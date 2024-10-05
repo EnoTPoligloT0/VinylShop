8a3f54b28ef5ca5519dd73d0259fd38679df4c99
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-05 05:20:10 +0200

    Animations on categories

--- frontend/components/Categories.tsx
+++ frontend/components/Categories.tsx
@@ -5,33 +5,49 @@ import Link from "next/link";
 function Categories() {
     return (
         <div className="container mx-auto">
-
             <div className="flex justify-between items-center mb-8">
                 <h1 className="text-3xl font-semibold">Popular Categories</h1>
                 <span className="flex items-center space-x-2 cursor-pointer">
                     <p className="font-medium text-royal-purple">View all</p>
-                    <Image src="/arrow.svg" alt="Arrow" width={24} height={24}/>
+                    <Image src="/arrow.svg" alt="Arrow" width={24} height={24} />
                 </span>
             </div>
 
             <div className="grid grid-cols-3 gap-1 md:gap-12">
-                <div className="flex justify-start ">
-                    <Image className={"cursor-pointer rounded-2xl"} src={"/rock-cat.png"} alt={"rock-alt"} height={424} width={424}/>
+                <div className="flex justify-start">
+                    <Image
+                        className="cursor-pointer rounded-2xl transform transition-transform duration-300 hover:scale-105"
+                        src="/rock-cat.png"
+                        alt="rock-alt"
+                        height={424}
+                        width={424}
+                    />
                 </div>
 
                 <div className="flex justify-center">
                     <Link href="/category/pop" passHref>
-                        <Image className={"cursor-pointer rounded-2xl"} src={"/pop-cat.png"} alt={"pop-alt"} height={424} width={424}/>
+                        <Image
+                            className="cursor-pointer rounded-2xl transform transition-transform duration-300 hover:scale-105"
+                            src="/pop-cat.png"
+                            alt="pop-alt"
+                            height={424}
+                            width={424}
+                        />
                     </Link>
                 </div>
 
                 <div className="flex justify-end">
                     <Link href="/category/jazz" passHref>
-                        <Image className={"cursor-pointer rounded-2xl"} src={"/jazz-cat.png"} alt={"jazz-alt"} height={424} width={424}/>
+                        <Image
+                            className="cursor-pointer rounded-2xl transform transition-transform duration-300 hover:scale-105"
+                            src="/jazz-cat.png"
+                            alt="jazz-alt"
+                            height={424}
+                            width={424}
+                        />
                     </Link>
                 </div>
             </div>
-
         </div>
     );
 }
