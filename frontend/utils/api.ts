e3fe38dd35193c406a39ebb590b8a3d9a87ab852
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-11 03:23:01 +0200

    Add Headers

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, 
     withCredentials: true,
+    headers:{
+        "Content-Type" : "application/json",
+    }
 });
 
 export default api;
