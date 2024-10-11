f1830128053b15551af53b8021a3221a8af687e3
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-10-11 02:52:49 +0200

    Enum status for Order, Payment, Shipment

---
+++ VinylShop.Core/Enums/Status.cs
@@ -0,0 +1,14 @@
+﻿namespace VinylShop.Core.Enums;
+
+//todo implement Status in Order,Payment,Shipment 
+public enum Status
+{
+    Pending,        
+    Confirmed,     
+    Shipped,       
+    Delivered,    
+    Canceled,    
+    Refunded,      
+    Failed,        
+    Processing    
+}
