6de73ba95f32523c5d3a0c1619e9a6efc5eb79e0
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-11-15 23:59:37 +0100

    Process stripe Payment method

--- VinylShop.Application/Services/PaymentService.cs
+++ VinylShop.Application/Services/PaymentService.cs
@@ -1,3 +1,5 @@
+using CSharpFunctionalExtensions;
+using Stripe;
 using VinylShop.Core.Interfaces.Repositories;
 using VinylShop.Core.Interfaces.Services;
 using VinylShop.Core.Models;
@@ -16,6 +18,29 @@ public class PaymentService : IPaymentService
     public async Task CreatePayment(Payment payment)
     {
         await _paymentRepository.Create(payment);
+    }
+
+    public async Task<Result<Payment>> ProcessStripePayment(Guid orderId, decimal amount, string paymentMethod, string currency = "usd")
+    {
+        var options = new PaymentIntentCreateOptions
+        {
+            Amount = Convert.ToInt64(amount * 100),
+            Currency = currency,
+            PaymentMethod = paymentMethod,
+            Confirm = true
+        };
+
+        var service = new PaymentIntentService();
+        var intent = await service.CreateAsync(options);
+
+        var paymentResult = Payment.Create(Guid.NewGuid(), orderId, DateTime.UtcNow, amount, paymentMethod, intent.Id);
+
+        if (!paymentResult.IsSuccess)
+            return Result.Failure<Payment>(paymentResult.Error);
+
+        await _paymentRepository.Create(paymentResult.Value);
+
+        return paymentResult;
     }
 
 
