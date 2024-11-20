29cad653b63fc106292f0c7548dfb2d94d3ae20c
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2024-11-20 02:22:22 +0100

    Wrap layout in stripePromise

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,13 @@
-import type {Metadata} from "next";
-import {Header, Footer} from "@/components";
+"use client";
+
+import {Footer, Header} from "@/components";
 import "./globals.css";
 import {CartProvider} from "@/context/CartContext";
 import {GoogleOAuthProvider} from "@react-oauth/google";
+import {Elements} from "@stripe/react-stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
 
-
-export const metadata: Metadata = {
-    title: "VinylShop",
-    description: "Your go-to vinyl shop",
-};
+const stripePromise = loadStripe("pk_test_51QKsJJHqGo0KeykHjiMci68gs5tv5Ym5wgt2WXb4zRHaID0V3AsQbjXSiuRJKD0FWBi9kH0LPtt6aZ37jac8azFa00OFZkimTs");
 
 export default function RootLayout({
                                        children,
@@ -30,11 +29,13 @@ export default function RootLayout({
         <body className="font-poppins antialiased">
 
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}>
-            <CartProvider>
-                <Header/>
-                <main>{children}</main>
-                <Footer/>
-            </CartProvider>
+            <Elements stripe={stripePromise}>
+                <CartProvider>
+                    <Header/>
+                    <main>{children}</main>
+                    <Footer/>
+                </CartProvider>
+            </Elements>
         </GoogleOAuthProvider>
         </body>
 
