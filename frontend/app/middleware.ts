0bd838cb8e1ff39b139c733f19d4f289ff2d33bd
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-01-07 23:54:48 +0100

    Middleware with admin root

---
+++ frontend/app/middleware.ts
@@ -0,0 +1,32 @@
+import {NextResponse} from 'next/server';
+import type {NextRequest} from 'next/server';
+import {jwtDecode} from "jwt-decode";
+
+export function middleware(req: NextRequest) {
+    const token = req.cookies.get('secretCookie')?.value;
+
+    if (!token) {
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+
+
+    try {
+        const payload: JwtPayload = jwtDecode(token);
+
+        console.log(payload);
+
+        if (payload.Role !== 'Admin') {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+
+        return NextResponse.next();
+    } catch (error) {
+        console.error('Invalid JWT:', error);
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+}
+
+export const config = {
+    matcher: ['/admin/:path*'],
+};
+
