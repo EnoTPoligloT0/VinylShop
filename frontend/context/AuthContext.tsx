bb5b2c4c5e938e380592387964b8856261b9829e
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-01-09 23:56:38 +0100

    SetIsAdmin logic

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -1,12 +1,13 @@
-'use client';
-
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import { jwtDecode } from 'jwt-decode';
+import { JwtPayload } from "@/types/jwtPayload";
 
 interface AuthContextType {
-    user: { userId: string; email?: string; Admin?: string } | null;
+    user: { userId: string; email?: string; Role?: string } | null;
     isLoggedIn: boolean;
+    isAdmin: boolean;
+    loading: boolean; // Add loading state
     login: (token: string) => void;
     logout: () => void;
 }
@@ -15,32 +16,40 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [user, setUser] = useState<AuthContextType['user']>(null);
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [loading, setLoading] = useState(true); // Initialize loading state
 
     useEffect(() => {
         const token = Cookies.get('secretCookie');
         if (token) {
             try {
                 const decoded: any = jwtDecode(token);
-                setUser({ userId: decoded.userId, email: decoded.email, Admin: decoded.Admin });
+                setUser({ userId: decoded.userId, email: decoded.email, Role: decoded.Role });
+                setIsAdmin(decoded.Role === 'Admin');
             } catch {
                 Cookies.remove('secretCookie');
+                setUser(null);
+                setIsAdmin(false);
             }
         }
+        setLoading(false); // Once the check is done, set loading to false
     }, []);
 
     const login = (token: string) => {
         Cookies.set('secretCookie', token, { expires: 1, path: '/', sameSite: 'Lax' });
         const decoded: any = jwtDecode(token);
-        setUser({ userId: decoded.userId, email: decoded.email, Admin: decoded.Admin });
+        setUser({ userId: decoded.userId, email: decoded.email, Role: decoded.Role });
+        setIsAdmin(decoded.Role === 'Admin');
     };
 
     const logout = () => {
         Cookies.remove('secretCookie');
         setUser(null);
+        setIsAdmin(false);
     };
 
     return (
-        <AuthContext.Provider value={{ user, isLoggedIn: !!user, login, logout }}>
+        <AuthContext.Provider value={{ user, isLoggedIn: !!user, isAdmin, loading, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
