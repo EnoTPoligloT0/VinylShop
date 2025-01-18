2d2acb300efaadaa22771cc708a399ccae5c73db
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-01-18 02:05:45 +0100

    GetOrdersResponsePagination record

---
+++ VinylShop.API/Contracts/Pagination/GetOrdersResponsePagination.cs
@@ -0,0 +1,5 @@
+﻿using VinylShop.API.Contracts.Orders;
+
+namespace VinylShop.API.Contracts.Pagination;
+
+public record GetOrdersResponsePagination(IEnumerable<GetOrdersResponse> Orders, PaginationInfo Pagination);
