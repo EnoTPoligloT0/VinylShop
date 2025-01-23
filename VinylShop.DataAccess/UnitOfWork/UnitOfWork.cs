fe05c3d88f910c46933c25eb1b0da83ff21f55fd
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-01-23 19:43:19 +0100

    Unit of work centralization implementation

---
+++ VinylShop.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using VinylShop.Core.Interfaces.Repositories;
+using VinylShop.Core.Interfaces.UnitOfWork;
+using VinylShop.DataAccess;
+
+namespace VinylShop.Application;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly VinylShopDbContext _context;
+
+    public UnitOfWork(VinylShopDbContext context)
+    {
+        _context = context;
+    }
+
+    public IOrderItemRepository OrderItems { get; }
+    public IOrderRepository Orders { get; }
+    public IPaymentRepository Payments { get; }
+    public IShipmentRepository Shipments { get; }
+    public IUserRepository Users { get; }
+    public IVinylRepository Vinyls { get; }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+    public async Task BeginTransactionAsync()
+    {
+         await _context.Database.BeginTransactionAsync();
+    }
+    public async Task CommitTransactionAsync()
+    {
+        await _context.Database.CommitTransactionAsync();
+    }
+    public async Task RollbackTransactionAsync()
+    {
+        await _context.Database.RollbackTransactionAsync();
+    }
+}
