b6c4d91281a8ac6a94624f96bfd3aff427a57beb
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-01-23 19:35:13 +0100

    Unit of work interface

---
+++ VinylShop.Core/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using VinylShop.Core.Interfaces.Repositories;
+
+namespace VinylShop.Core.Interfaces.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    IOrderItemRepository OrderItems { get; }
+    IOrderRepository Orders { get; }
+    IPaymentRepository Payments { get; }
+    IShipmentRepository Shipments { get; }
+    IUserRepository Users { get; }
+    IVinylRepository Vinyls { get; }
+
+    Task<bool> SaveChangesAsync();
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RollbackTransactionAsync();
+}
